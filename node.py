eb8298a5bc54c56381e58e68887271647b8f6677
Sivaram Yadav Nalliboyana <nsivaramdav@gmail.com>
2026-03-22 20:03:33 +0100

    Added node class

---
+++ node.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self, id , vector, max_level, connections):
+        self.id = id
+        self.vector = vector
+        self.max_level = max_level
+        self.connections = connections
+
